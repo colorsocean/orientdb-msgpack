5b4d35d9f3bcd0f02a6739e8c2ddfe6939f37cfd
maggiolo00 <maggiolo00@gmail.com>
2014-12-27 15:28:59 +0100

    first impl typ basic

--- src/main/java/com/orientechnologies/msgpack/serializer/ORecordSerializerMessagePack.java
+++ src/main/java/com/orientechnologies/msgpack/serializer/ORecordSerializerMessagePack.java
@@ -18,35 +18,216 @@
 
 package com.orientechnologies.msgpack.serializer;
 
+import com.orientechnologies.common.log.OLogManager;
+import com.orientechnologies.orient.core.metadata.schema.OProperty;
+import com.orientechnologies.orient.core.metadata.schema.OType;
 import com.orientechnologies.orient.core.record.ORecord;
+import com.orientechnologies.orient.core.record.impl.ODocument;
+import com.orientechnologies.orient.core.record.impl.ODocumentInternal;
 import com.orientechnologies.orient.core.serialization.serializer.record.ORecordSerializer;
+import org.msgpack.core.MessageFormat;
+import org.msgpack.core.MessagePack;
+import org.msgpack.core.MessagePacker;
+import org.msgpack.core.MessageUnpacker;
+import org.msgpack.value.holder.ValueHolder;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by Enrico Risa on 24/12/14.
  */
 public class ORecordSerializerMessagePack implements ORecordSerializer {
 
-  public static final String                       NAME     = "ORecordSerializerMessagePack";
+    public static final String NAME = "ORecordSerializerMessagePack";
 
-  public static final ORecordSerializerMessagePack INSTANCE = new ORecordSerializerMessagePack();
+    public static final ORecordSerializerMessagePack INSTANCE = new ORecordSerializerMessagePack();
 
-  @Override
-  public ORecord fromStream(byte[] iSource, ORecord iRecord, String[] iFields) {
-    return null;
-  }
+    @Override
+    public ORecord fromStream(byte[] iSource, ORecord iRecord, String[] iFields) {
 
-  @Override
-  public byte[] toStream(ORecord iSource, boolean iOnlyDelta) {
-    return new byte[0];
-  }
+        MessageUnpacker unpacker = MessagePack.newDefaultUnpacker(iSource);
 
-  @Override
-  public int getCurrentVersion() {
-    return 0;
-  }
+        try {
+            deserializeDocument(unpacker, (ODocument) iRecord);
+        } catch (IOException e) {
+            OLogManager.instance().error(this, "Error deserializing");
+        }
+        return iRecord;
+    }
 
-  @Override
-  public int getMinSupportedVersion() {
-    return 0;
-  }
+    @Override
+    public byte[] toStream(ORecord iSource, boolean iOnlyDelta) {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        MessagePacker packer = MessagePack.newDefaultPacker(out);
+        try {
+            serializeClass(iSource, packer);
+            serializeDocument((ODocument) iSource, packer);
+            packer.close();
+            return out.toByteArray();
+        } catch (IOException e) {
+            OLogManager.instance().error(this, "Error serializing document");
+            throw new RuntimeException(e);
+        }
+    }
+
+    /* Serializer */
+    private void serializeClass(ORecord iSource, MessagePacker packer) throws IOException {
+        ODocument doc = (ODocument) iSource;
+        String className = doc.getClassName();
+        packer.packString(className != null ? className : "");
+    }
+
+    private void serializeDocument(ODocument document, MessagePacker packer) throws IOException {
+
+        Map<String, OProperty> properties = new HashMap<>();
+        for (Map.Entry<String, Object> entry : document) {
+            OType type = getFieldType(document, entry.getKey(), entry.getValue(), properties);
+
+            writeSingleValue(packer, entry.getKey(), entry.getValue(), type);
+        }
+    }
+
+    private void writeSingleValue(MessagePacker packer, String key, Object value, OType type) throws IOException {
+
+        packer.packString(key);
+
+        if (type == null || value == null) {
+            packer.packNil();
+            return;
+        }
+
+        switch (type) {
+            case INTEGER:
+
+                packer.packInt(((Number) value).intValue());
+                break;
+            case LONG:
+                packer.packLong(((Number) value).longValue());
+                break;
+            case SHORT:
+                packer.packShort(((Number) value).shortValue());
+                break;
+            case STRING:
+                packer.packString("" + value);
+                break;
+            case DOUBLE:
+                packer.packDouble(((Number) value).doubleValue());
+                break;
+            case FLOAT:
+                packer.packFloat(((Number) value).floatValue());
+                break;
+            case BYTE:
+                packer.packByte((Byte) value);
+                break;
+            case BOOLEAN:
+                packer.packBoolean((Boolean) value);
+                break;
+            case DATETIME:
+
+                break;
+            case DATE:
+
+                break;
+            case EMBEDDED:
+                break;
+            case EMBEDDEDSET:
+            case EMBEDDEDLIST:
+                break;
+            case DECIMAL:
+                BigDecimal decimalValue = (BigDecimal) value;
+                packer.packBigInteger(decimalValue.unscaledValue());
+                break;
+            case BINARY:
+                byte[] bytes = (byte[]) value;
+                packer.packBinaryHeader(bytes.length);
+                packer.writePayload(bytes);
+                break;
+            case LINKSET:
+            case LINKLIST:
+                break;
+            case LINK:
+                break;
+            case LINKMAP:
+                break;
+            case EMBEDDEDMAP:
+                break;
+            case LINKBAG:
+                break;
+            case CUSTOM:
+                break;
+            case TRANSIENT:
+                break;
+            case ANY:
+                break;
+        }
+    }
+
+    private OType getFieldType(final ODocument document, final String key, final Object fieldValue,
+                               final Map<String, OProperty> properties) {
+        OType type = document.fieldType(key);
+        if (type == null) {
+            final OProperty prop = properties != null ? properties.get(key) : null;
+            if (prop != null)
+                type = prop.getType();
+
+            if (type == null || OType.ANY == type)
+                type = OType.getTypeByValue(fieldValue);
+        }
+        return type;
+    }
+
+    private void deserializeDocument(MessageUnpacker unpacker, ODocument document) throws IOException {
+
+        String className = unpacker.unpackString();
+        if (className != null && !className.isEmpty())
+            ODocumentInternal.fillClassNameIfNeeded(document, className);
+
+        String fieldName = null;
+        Object value = null;
+        ValueHolder v = null;
+        MessageFormat format = null;
+        while (unpacker.hasNext()) {
+
+            v = new ValueHolder();
+            unpacker.unpackValue(v);
+            fieldName = v.get().asString().toString();
+            v = new ValueHolder();
+            format = unpacker.unpackValue(v);
+            switch (format.getValueType()) {
+
+                case INTEGER:
+                    value = v.get().asInteger().toInt();
+                    break;
+                case BOOLEAN:
+                    value = v.get().asBoolean().toBoolean();
+                    break;
+                case FLOAT:
+                    value = v.get().asFloat().toFloat();
+                    break;
+                case STRING:
+                    value = v.get().asString().toString();
+                    break;
+
+                case BINARY:
+                    value = v.get().asBinary().toByteArray();
+
+            }
+
+            document.field(fieldName, value);
+        }
+    }
+
+    @Override
+    public int getCurrentVersion() {
+        return 0;
+    }
+
+    @Override
+    public int getMinSupportedVersion() {
+        return 0;
+    }
 }
--- src/test/java/com/orientechonologies/msgpack/ODocumentSchemalessMsgPackTest.java
+++ src/test/java/com/orientechonologies/msgpack/ODocumentSchemalessMsgPackTest.java
@@ -19,16 +19,11 @@
 package com.orientechonologies.msgpack;
 
 import com.orientechnologies.msgpack.serializer.ORecordSerializerMessagePack;
-import com.orientechnologies.orient.core.db.record.ridbag.ORidBag;
-import com.orientechnologies.orient.core.id.ORecordId;
-import com.orientechnologies.orient.core.metadata.schema.OType;
 import com.orientechnologies.orient.core.record.impl.ODocument;
 import org.testng.annotations.Test;
 
-import java.math.BigDecimal;
 import java.util.Arrays;
 import java.util.Calendar;
-import java.util.Date;
 
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertNull;
@@ -38,75 +33,71 @@ import static org.testng.Assert.assertNull;
  */
 public class ODocumentSchemalessMsgPackTest {
 
-  protected ORecordSerializerMessagePack serializer;
+    protected ORecordSerializerMessagePack serializer;
 
-  public ODocumentSchemalessMsgPackTest() {
-    serializer = new ORecordSerializerMessagePack();
-  }
+    public ODocumentSchemalessMsgPackTest() {
+        serializer = new ORecordSerializerMessagePack();
+    }
 
-  @Test
-  public void testSimpleDocument() {
+    @Test
+    public void testSimpleDocument() {
 
-      ODocument document = new ODocument();
+        ODocument document = new ODocument();
 
-      document.field("name", "name");
-      document.field("age", 20);
-      document.field("youngAge", (short) 20);
-      document.field("oldAge", (long) 20);
-      document.field("heigth", 12.5f);
-      document.field("bitHeigth", 12.5d);
-      document.field("class", (byte) 'C');
-      document.field("nullField", (Object) null);
-      document.field("character", 'C');
-      document.field("alive", true);
-      document.field("dateTime", new Date());
-      document.field("bigNumber", new BigDecimal("43989872423376487952454365232141525434.32146432321442534"));
-      ORidBag bag = new ORidBag();
-      bag.add(new ORecordId(1, 1));
-      bag.add(new ORecordId(2, 2));
-      // document.field("ridBag", bag);
-      Calendar c = Calendar.getInstance();
-      document.field("date", c.getTime(), OType.DATE);
-      Calendar c1 = Calendar.getInstance();
-      c1.set(Calendar.MILLISECOND, 0);
-      c1.set(Calendar.SECOND, 0);
-      c1.set(Calendar.MINUTE, 0);
-      c1.set(Calendar.HOUR_OF_DAY, 0);
-      document.field("date1", c1.getTime(), OType.DATE);
+        document.field("name", "name");
+        document.field("age", 20);
+        document.field("youngAge", (short) 20);
+        document.field("oldAge", (long) 20);
+        document.field("heigth", 12.5f);
+        document.field("bitHeigth", 12.5d);
+        document.field("class", (byte) 'C');
+        document.field("nullField", (Object) null);
+        document.field("character", 'C');
+        document.field("alive", true);
+//        document.field("dateTime", new Date());
+//        document.field("bigNumber", new BigDecimal("43989872423376487952454365232141525434.32146432321442534"));
+        Calendar c = Calendar.getInstance();
+//      document.field("date", c.getTime(), OType.DATE);
+        Calendar c1 = Calendar.getInstance();
+        c1.set(Calendar.MILLISECOND, 0);
+        c1.set(Calendar.SECOND, 0);
+        c1.set(Calendar.MINUTE, 0);
+        c1.set(Calendar.HOUR_OF_DAY, 0);
+//      document.field("date1", c1.getTime(), OType.DATE);
 
-      byte[] byteValue = new byte[10];
-      Arrays.fill(byteValue, (byte) 10);
-      document.field("bytes", byteValue);
+        byte[] byteValue = new byte[10];
+        Arrays.fill(byteValue, (byte) 10);
+        document.field("bytes", byteValue);
 
-      document.field("utf8String", new String("A" + "\u00ea" + "\u00f1" + "\u00fc" + "C"));
-      document.field("recordId", new ORecordId(10, 10));
+        document.field("utf8String", new String("A" + "\u00ea" + "\u00f1" + "\u00fc" + "C"));
+//      document.field("recordId", new ORecordId(10, 10));
 
-      byte[] res = serializer.toStream(document, false);
-      ODocument extr = (ODocument) serializer.fromStream(res, new ODocument(), new String[] {});
+        byte[] res = serializer.toStream(document, false);
+        ODocument extr = (ODocument) serializer.fromStream(res, new ODocument(), new String[]{});
 
-      c.set(Calendar.MILLISECOND, 0);
-      c.set(Calendar.SECOND, 0);
-      c.set(Calendar.MINUTE, 0);
-      c.set(Calendar.HOUR_OF_DAY, 0);
+        c.set(Calendar.MILLISECOND, 0);
+        c.set(Calendar.SECOND, 0);
+        c.set(Calendar.MINUTE, 0);
+        c.set(Calendar.HOUR_OF_DAY, 0);
 
-      assertEquals(extr.fields(), document.fields());
-      assertEquals(extr.field("name"), document.field("name"));
-      assertEquals(extr.field("age"), document.field("age"));
-      assertEquals(extr.field("youngAge"), document.field("youngAge"));
-      assertEquals(extr.field("oldAge"), document.field("oldAge"));
-      assertEquals(extr.field("heigth"), document.field("heigth"));
-      assertEquals(extr.field("bitHeigth"), document.field("bitHeigth"));
-      assertEquals(extr.field("class"), document.field("class"));
-      // TODO fix char management issue:#2427
-      // assertEquals(document.field("character"), extr.field("character"));
-      assertEquals(extr.field("alive"), document.field("alive"));
-      assertEquals(extr.field("dateTime"), document.field("dateTime"));
-      assertEquals(extr.field("date"), c.getTime());
-      assertEquals(extr.field("date1"), c1.getTime());
-      assertEquals(extr.field("bytes"), document.field("bytes"));
-      assertEquals(extr.field("utf8String"), document.field("utf8String"));
-      assertEquals(extr.field("recordId"), document.field("recordId"));
-      assertEquals(extr.field("bigNumber"), document.field("bigNumber"));
-      assertNull(extr.field("nullField"));
-  }
+        assertEquals(extr.fields(), document.fields());
+        assertEquals(extr.field("name"), document.field("name"));
+        assertEquals(extr.field("age"), document.field("age"));
+        assertEquals(extr.field("youngAge"), document.field("youngAge"));
+        assertEquals(extr.field("oldAge"), document.field("oldAge"));
+        assertEquals(extr.field("heigth"), document.field("heigth"));
+        assertEquals(extr.field("bitHeigth"), document.field("bitHeigth"));
+        assertEquals(extr.field("class"), document.field("class"));
+        // TODO fix char management issue:#2427
+        // assertEquals(document.field("character"), extr.field("character"));
+        assertEquals(extr.field("alive"), document.field("alive"));
+        assertEquals(extr.field("dateTime"), document.field("dateTime"));
+        assertEquals(extr.field("date"), c.getTime());
+        assertEquals(extr.field("date1"), c1.getTime());
+        assertEquals(extr.field("bytes"), document.field("bytes"));
+        assertEquals(extr.field("utf8String"), document.field("utf8String"));
+        assertEquals(extr.field("recordId"), document.field("recordId"));
+        assertEquals(extr.field("bigNumber"), document.field("bigNumber"));
+        assertNull(extr.field("nullField"));
+    }
 }
